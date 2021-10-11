2fd4572c5e82254b991f58b7b836b16bda85bafd
Enea Muskaj <eneamuskaj@gmail.com>
2021-10-10 21:17:50 -0500

    fixed modules

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const _ = require('lodash')
 
 const server = http.createServer((req, res) => {
-  //lodash
+  //lodash //
   const num = _.random(0, 20)
   console.log(num)
 
