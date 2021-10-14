24b7fac11ad4baf56590118df7f03f42b160c214
eneamuskaj <eneamuskaj@gmail.com>
2021-10-14 07:43:01 -0500

    using get, post and delete routes

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express()
 //coonect to mongo db
 
 const dbURI =
-  'mongodb+srv://@cluster1.spdks.mongodb.net/node-tuts?retryWrites=true&w=majority'
+  'mongodb+srv://eneamuskaj:test1234@cluster1.spdks.mongodb.net/node-tuts?retryWrites=true&w=majority'
 mongoose
   .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => {
@@ -24,9 +24,8 @@ app.set('view engine', 'ejs')
 //middleware static files
 
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
 app.use(morgan('dev'))
-
-//mongoose and mongo sandbox
 
 app.get('/', (req, res) => {
   res.redirect('/blogs')
@@ -50,10 +49,41 @@ app.get('/blogs', (req, res) => {
     })
 })
 
-//blog routes
 app.get('/blogs/create', (req, res) => {
   res.render('create', { title: 'Create a new blog' })
 })
+
+app.post('/blogs', (req, res) => {
+  const blog = new Blog (req.body)
+  blog.save().then((result)=> {
+    res.redirect("/blogs")
+  }).catch((err)=> {
+    console.log(err)
+  })
+})
+
+app.get('/blogs/:id', (req, res) => {
+  const id= req.params.id;
+  Blog.findById(id)
+  .then(result => {
+    res.render("details", {blog: result, title: "Blog Details"})
+  }).catch(err => {
+    console.log(err)
+  })
+})
+
+app.delete("/blogs/:id", (req, res)=> {
+  const id = req.params.id;
+  Blog.findByIdAndDelete(id).then(result => {
+    res.json({redirect: "/blogs"})
+  })
+  .catch((err)=> {
+    console.log(err)
+  })
+})
+
+//blog routes
+
 
 // 404 page
 app.use((req, res) => {
